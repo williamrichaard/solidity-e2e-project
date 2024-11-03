e1ea96c422cbf049ec884f9e6719d62dc2482a70
William Richard <wrichaard@hotmail.com>
2024-11-02 22:24:22 -0300

    feat: implement NFT smart contract with focus on data types and variable handling

--- DataTypes.sol
+++ DataTypes.sol
@@ -1,51 +1,64 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
+/// @title MyToken - A simple ERC-721-like NFT contract
+/// @notice This contract follows the ERC-721 standard for NFTs, with basic minting and ownership functionality.
+/// @dev This contract is not fully ERC-721 compliant but mimics the basic functionality for educational purposes.
 contract MyToken {
+    // Public variables for the token name and symbol
     string public name = "MyToken";
     string public symbol = "MTK";
+
+    // Counter to keep track of the next token ID to be minted
     uint256 private _tokenIdCounter;
+
+    // Address of the contract owner
     address private _owner;
 
-    // Mapping to track token ownership
+    // Mapping to track token ownership by token ID
     mapping(uint256 => address) private _owners;
 
-    // Mapping to track minted tokens
+    // Mapping to track whether a token has been minted
     mapping(uint256 => bool) private _mintedTokens;
 
-    // Event to emit when a token is transferred
+    // Event to emit when a token is transferred (including minting)
     event Transfer(
         address indexed from,
         address indexed to,
         uint256 indexed tokenId
     );
 
-    // Event to emit when ownership is transferred
+    // Event to emit when ownership of the contract is transferred
     event OwnershipTransferred(
         address indexed previousOwner,
         address indexed newOwner
     );
 
+    /// @notice Constructor to initialize the contract with an initial owner
+    /// @param initialOwner The address of the initial owner of the contract
     constructor(address initialOwner) {
         _owner = initialOwner;
         _tokenIdCounter = 0;
         emit OwnershipTransferred(address(0), initialOwner);
     }
 
-    // Modifier to restrict access to the owner
+    // Modifier to restrict access to the owner of the contract
     modifier onlyOwner() {
         require(msg.sender == _owner, "Caller is not the owner");
         _;
     }
 
-    // Function to transfer ownership
+    /// @notice Function to transfer ownership of the contract
+    /// @param newOwner The address of the new owner
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
 
-    // Function to mint a new NFT
+    /// @notice Function to mint a new NFT
+    /// @dev Only the owner of the contract can mint new tokens
+    /// @param to The address to which the new token will be minted
     function mintNFT(address to) public onlyOwner {
         require(
             to != address(0),
@@ -53,7 +66,7 @@ contract MyToken {
         );
         uint256 newTokenId = _tokenIdCounter;
 
-        // Assign ownership of the token
+        // Assign ownership of the token to the 'to' address
         _owners[newTokenId] = to;
 
         // Mark the token as minted
@@ -62,10 +75,13 @@ contract MyToken {
         // Emit the transfer event (from address(0) to indicate minting)
         emit Transfer(address(0), to, newTokenId);
 
+        // Increment the token ID counter for the next mint
         _tokenIdCounter += 1;
     }
 
-    // Function to get the owner of a specific token ID
+    /// @notice Function to get the owner of a specific token ID
+    /// @param tokenId The ID of the token to query
+    /// @return The address of the owner of the token
     function getOwner(uint256 tokenId) public view returns (address) {
         require(
             _mintedTokens[tokenId],
@@ -74,12 +90,15 @@ contract MyToken {
         return _owners[tokenId];
     }
 
-    // Function to check if a token exists
+    /// @notice Internal function to check if a token exists
+    /// @param tokenId The ID of the token to check
+    /// @return True if the token exists, false otherwise
     function _exists(uint256 tokenId) internal view returns (bool) {
         return _mintedTokens[tokenId];
     }
 
-    // Function to get the current owner of the contract
+    /// @notice Function to get the current owner of the contract
+    /// @return The address of the current owner of the contract
     function owner() public view returns (address) {
         return _owner;
     }
